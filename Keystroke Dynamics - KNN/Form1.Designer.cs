7275e42b8795bdc1df1b93f693e5db6644894df1
s.malyszko1@gmail.com <s.malyszko1@gmail.com>
2019-05-28 11:36:53 +0200

    Prawie skońćzone pobieranie próbek, zapis do pliku, mierzenie czasu.

--- Keystroke Dynamics - KNN/Form1.Designer.cs
+++ Keystroke Dynamics - KNN/Form1.Designer.cs
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.WpisywanieTextBox = new System.Windows.Forms.TextBox();
+            this.NickTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ZatwierdzButton = new System.Windows.Forms.Button();
+            this.CzastextBox = new System.Windows.Forms.TextBox();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(283, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIOMETRIA JEST NAJLEPSZA";
+            // 
+            // WpisywanieTextBox
+            // 
+            this.WpisywanieTextBox.Location = new System.Drawing.Point(223, 85);
+            this.WpisywanieTextBox.Name = "WpisywanieTextBox";
+            this.WpisywanieTextBox.Size = new System.Drawing.Size(319, 22);
+            this.WpisywanieTextBox.TabIndex = 1;
+            this.WpisywanieTextBox.TextChanged += new System.EventHandler(this.WpisywanieTextBox_TextChanged);
+            this.WpisywanieTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.WpisywanieTextBox_KeyDown);
+            this.WpisywanieTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.WpisywanieTextBox_KeyUp);
+            // 
+            // NickTextBox
+            // 
+            this.NickTextBox.Location = new System.Drawing.Point(223, 143);
+            this.NickTextBox.Name = "NickTextBox";
+            this.NickTextBox.Size = new System.Drawing.Size(319, 22);
+            this.NickTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wpisz zdanie:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Wpisz nick:";
+            // 
+            // ZatwierdzButton
+            // 
+            this.ZatwierdzButton.Location = new System.Drawing.Point(337, 198);
+            this.ZatwierdzButton.Name = "ZatwierdzButton";
+            this.ZatwierdzButton.Size = new System.Drawing.Size(88, 23);
+            this.ZatwierdzButton.TabIndex = 5;
+            this.ZatwierdzButton.Text = "Zatwierdz";
+            this.ZatwierdzButton.UseVisualStyleBackColor = true;
+            this.ZatwierdzButton.Click += new System.EventHandler(this.ZatwierdzButton_Click);
+            // 
+            // CzastextBox
+            // 
+            this.CzastextBox.Location = new System.Drawing.Point(223, 291);
+            this.CzastextBox.Name = "CzastextBox";
+            this.CzastextBox.Size = new System.Drawing.Size(319, 22);
+            this.CzastextBox.TabIndex = 6;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AllowDrop = true;
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(548, 85);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(65, 19);
+            this.errorLabel.TabIndex = 7;
+            this.errorLabel.Text = "ERROR";
+            this.errorLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.CzastextBox);
+            this.Controls.Add(this.ZatwierdzButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NickTextBox);
+            this.Controls.Add(this.WpisywanieTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox WpisywanieTextBox;
+        private System.Windows.Forms.TextBox NickTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ZatwierdzButton;
+        private System.Windows.Forms.TextBox CzastextBox;
+        private System.Windows.Forms.Label errorLabel;
     }
 }
 
--- Keystroke Dynamics - KNN/Form1.cs
+++ Keystroke Dynamics - KNN/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,94 @@ namespace Keystroke_Dynamics___KNN
         public Form1()
         {
             InitializeComponent();
+        }
+        Stopwatch sw = new Stopwatch();
+        string haslo = "BIOMETRIA JEST NAJLEPSZA";
+        int[] profil = new int[27];
+        int[] profilLicznik = new int[27];
+        private void WpisywanieTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+            if(WpisywanieTextBox.Text.Length!=0 && WpisywanieTextBox.Text[WpisywanieTextBox.Text.Length-1]!=haslo[WpisywanieTextBox.Text.Length-1])
+            {
+                ClearTB();
+            }
+            else
+            {
+                errorLabel.Visible = false;
+            }
+        }
+        private void ClearTB()
+        {
+            WpisywanieTextBox.Text = "";
+            errorLabel.Visible = true;
+            for (int i = 0; i < 27; i++)
+                profil[i] = 0;
+
+        }
+        private void WpisywanieTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            sw.Start();
+
+        }
+
+        private void WpisywanieTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            sw.Stop();
+            //Spacja
+            if (WpisywanieTextBox.Text.Length != 0 && WpisywanieTextBox.Text[WpisywanieTextBox.Text.Length - 1] == ' ')
+            {
+                profil[26] += sw.Elapsed.Milliseconds;
+                profilLicznik[26]++;
+            }
+            //Literka
+            else if (WpisywanieTextBox.Text.Length != 0)
+            {
+                int literka = (int)WpisywanieTextBox.Text[WpisywanieTextBox.Text.Length - 1] - 65;
+                profil[literka] += sw.Elapsed.Milliseconds;
+                profilLicznik[literka]++;
+            }
+            sw.Reset();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 27; i++)
+            {
+                profil[i] = 0;
+                profilLicznik[i] = 0;
+            }
+        }
+        private void liczSrednia()
+        {
+            for(int i=0;i<27;i++)
+                if(profilLicznik[i]>1)
+                {
+                    profil[i] /= profilLicznik[i];
+                }
+        }
+        private void generujPlik()
+        {
+            int licznik = 0;            
+            string fileName = "C:\\Image\\" + NickTextBox.Text.ToString() + licznik.ToString()+ ".txt";
+            string wszystko = "";
+            for(int i=0;i<27;i++)
+            {
+                wszystko += i.ToString() + ". " + profil[i] +"-"+ (char)(i+65) + Environment.NewLine;
+            }
+            while(File.Exists(fileName))
+            {
+                licznik++;
+                fileName = "C:\\Image\\" + NickTextBox.Text.ToString() + licznik.ToString() + ".txt";
+            }
+        
+            File.WriteAllText(fileName, wszystko);
+           
+        }
+        private void ZatwierdzButton_Click(object sender, EventArgs e)
+        {
+            liczSrednia();
+            generujPlik();
         }
     }
 }
