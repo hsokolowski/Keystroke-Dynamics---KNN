1feeadfe71bce0add7f3e95ab0fa03373243fbd1
s.malyszko1@gmail.com <s.malyszko1@gmail.com>
2019-06-03 18:30:14 +0200

    Ilosc probek/uzytkownikow pobierana z pliku i aktualizowana po dodaniu próbki.

--- Keystroke Dynamics - KNN/Form1.cs
+++ Keystroke Dynamics - KNN/Form1.cs
@@ -23,6 +23,8 @@ namespace Keystroke_Dynamics___KNN
         int[] profil = new int[27];
         int[] profilLicznik = new int[27];
         int[] klasy;
+        string iloscProbekTxt = "C:\\Biometria\\iloscProbek.txt";
+        string iloscUzytkownikowTxt = "C:\\Biometria\\iloscUzytkownikow.txt";
         int iloscUzytkownikow=0, iloscProbek=0;
         string[] nicki;
         private void WpisywanieTextBox_TextChanged(object sender, EventArgs e)
@@ -88,10 +90,35 @@ namespace Keystroke_Dynamics___KNN
                 profil[i] = 0;
                 profilLicznik[i] = 0;
             }
-            for (int i = 0; i < 30; i++)
+            for (int i = 0; i < iloscProbek; i++)
             {
                 //klasy[i] = -1;
                 //if (i < 10) nicki[i] = " ";
+            }
+            
+            using (StreamReader file = new StreamReader(iloscProbekTxt))
+            {
+                string ilosc;
+                while ((ilosc = file.ReadLine()) != null)
+                {
+
+                    CzastextBox.Text = ilosc;
+                    iloscProbek = Convert.ToInt32(ilosc);
+                    
+                }
+                file.Close();
+            }
+            using (StreamReader file = new StreamReader(iloscUzytkownikowTxt))
+            {
+                string ilosc;
+                while ((ilosc = file.ReadLine()) != null)
+                {
+
+                    CzastextBox.Text = ilosc;
+                    iloscUzytkownikow = Convert.ToInt32(ilosc);
+
+                }
+                file.Close();
             }
         }
         private void liczSrednia()
@@ -138,6 +165,9 @@ namespace Keystroke_Dynamics___KNN
                 zaktualizujBaze();
                 generujPlik(); // dodanie do bazy
                 ++iloscProbek;
+                ++iloscUzytkownikow;
+                File.WriteAllText("C:\\Biometria\\iloscUzytkownikow.txt",iloscUzytkownikow.ToString());
+                File.WriteAllText("C:\\Biometria\\iloscProbek.txt", iloscProbek.ToString());
                 ClearTB();
                 motywujacyLabel.Text = "Elegancko";
 
@@ -309,17 +339,17 @@ namespace Keystroke_Dynamics___KNN
             int indeks=0;
             int max=-1,maxindeks=0;
             iloscMax = 0;
-            int[] tmp = new int[10];
-            for (int i=0; i<10; i++) // zerujemy tablice ktora zlicza ilosc wystopienia klas
+            int[] tmp = new int[iloscUzytkownikow];
+            for (int i=0; i< iloscUzytkownikow; i++) // zerujemy tablice ktora zlicza ilosc wystopienia klas
             {
                 tmp[i] = 0;
             }
-            for(int i=0; i<10; i++) // tu ustalamy nasze k
+            for(int i=0; i< iloscUzytkownikow; i++) // tu ustalamy nasze k
             {
                 tmp[lista[i].Item2]++;
             }
 
-            for (int i = 0; i < 10; i++) // sprawdzamy ktora klasa najczesniej wystapila
+            for (int i = 0; i < iloscUzytkownikow; i++) // sprawdzamy ktora klasa najczesniej wystapila
             {
                 if (tmp[i] > max) {
 
@@ -328,7 +358,7 @@ namespace Keystroke_Dynamics___KNN
                 }
                
             }
-            for (int i = 0; i < 10; i++) // sprawdzamy czy sie powtarza
+            for (int i = 0; i < iloscUzytkownikow; i++) // sprawdzamy czy sie powtarza
             {
                 if (tmp[i] == max) iloscMax++;
             }
@@ -344,8 +374,8 @@ namespace Keystroke_Dynamics___KNN
             List<Tuple<int, int>> listaDosprawdzeniaMan, listaDosprawdzeniaEuk, listaDosprawdzeniaCze;
             string nickDoSprawdzenia;
             int[] y;
-            int[] nazwyKlas=new int[30];
-            int[] odleglosciMan = new int[30], odleglosciEuk = new int[30], odleglosciCze = new int[30];
+            int[] nazwyKlas=new int[iloscProbek];
+            int[] odleglosciMan = new int[iloscProbek], odleglosciEuk = new int[iloscProbek], odleglosciCze = new int[iloscProbek];
             int kursorDoTablicy = 0;
             int licznikDoNicku = 0;
             string listaWektorow = "C:\\Biometria\\wektorKlas.txt";
